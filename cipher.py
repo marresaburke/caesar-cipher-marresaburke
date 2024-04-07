3fbd050a06a6622e0f10086694dfe70e2f46ef99
marresaburke <marresa.burke@gmail.com>
2024-04-07 18:42:15 -0400

    Update cipher.py
    I used dictionary to solve the Caesar Cypher. The alphabet variable holds the letters (key) and their new value. For every letter the user inputs, the code will find the letter in the dictionary and add it to the variable secret message. Once its complete, the program will spit out the secret message.

--- cipher.py
+++ cipher.py
@@ -1,1 +1,9 @@
-# add your code here
+alphabet = {"a":"f", "b":"g", "c":"h", "d":"i", "e":"j", "f":"k", "g":"l", "h":"m", "i":"n" , "j":"o", "k":"p", "l":"q", "m":"r", "n":"s", "o":"t", "p":"u", "q":"v", "r":"w", "s":"x", "t":"y", "u":"z", "v":"a", "w":"b", "x":"c", "y":"d", "z":"e", " ":"$"}
+user_input = input("Tell me a secret.")
+user_input = user_input.lower()
+secret_msg = ""
+
+for letter in user_input: 
+  secret_msg += alphabet[letter]
+   
+print("Your secret message is:",secret_msg)
